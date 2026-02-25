948f11da733af05f23c8ba2fcaff034ec63d846d
Endks <mientrasde78@gmail.com>
2026-02-25 16:38:08 -0500

    Variable env

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mega-data.onrender.com/api/blog/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// 👉 INTERCEPTOR: añade el token a TODAS las requests
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
